fb1635d517a4ee3bbb15d70f524c0dbbc70d373e
Jubipils <schenk.leopold@gmail.com>
2026-05-07 12:54:11 +0200

    Add files via upload

--- Praktika/ai_bandit/lab1/Bandit.java
+++ Praktika/ai_bandit/lab1/Bandit.java
@@ -1,21 +1,71 @@
 package ai_bandit.lab1;
 
+import java.util.Random;
+
 public class Bandit {
-	private static Random random;
-	private String name;
-	private double pricePerRound;
-	private double averageWin;
-	private double stdDevWin;
-	private double overallProfit;
-	private int roundsPlayerd;
-	
-	public Bandit(String name, double pricePerRound, double averageWin, double stdDevWin) {
-		this.name = name;
-		this(pricePerRound, averageWin, stdDevWin);
+
+		private Random random = new Random();
+		private String name;
+		private double pricePerRound;
+		private double averageWin;
+		private double stdDevWin;
+		private double overallProfit;
+		private int roundsPlayed;
+		
+		public Bandit(String name, double pricePerRound, double averageWin, double stdDevWin) {
+			super();
+			this.name = name;
+			this.pricePerRound = pricePerRound;
+			this.averageWin = averageWin;
+			this.stdDevWin = stdDevWin;
+		}
+
+		public Bandit(double pricePerRound, double averageWin, double stdDevWin) {
+			super();
+			this.pricePerRound = pricePerRound;
+			this.averageWin = averageWin;
+			this.stdDevWin = stdDevWin;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public double getPricePerRound() {
+			return pricePerRound;
+		}
+
+		public double getOverallProfit() {
+			return overallProfit;
+		}
+
+		public int getRoundsPlayed() {
+			return roundsPlayed;
+		}
+		
+		public double getMeanProfitPerRound() {
+			
+			if(roundsPlayed <= 0)
+				return 0;
+			else
+				return overallProfit/roundsPlayed;
+		}
+		
+		public double play() {
+			double win = this.determineWin();
+			this.overallProfit += this.pricePerRound - win;
+			this.roundsPlayed++;
+			return win;
+		}
+		
+		private double determineWin() {
+
+			double win = this.averageWin + this.random.nextGaussian() * this.stdDevWin;
+			double rest = win % 0.1;
+			win -= rest;
+			
+			return win;
+			
+		}
+		
 	}
-	public Bandit(double pricePerRound, double averageWin, double stdDevWin) {
-		this.pricePerRound = pricePerRound;
-		this.averageWin = averageWin;
-		this.stdDevWin = stdDevWin;
-	}
-}
